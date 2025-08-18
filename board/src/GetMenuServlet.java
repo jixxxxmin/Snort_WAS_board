091682c49fe28899c15e2c880fdb8beaff91a331
Min <112598226+jixxxxmin@users.noreply.github.com>
2025-08-18 15:01:51 +0900

    Update GetMenuServlet.java

--- board/src/GetMenuServlet.java
+++ board/src/GetMenuServlet.java
@@ -12,6 +12,24 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         
-        
+        String DB_URL = System.getenv("DB_URL");
+        String DB_USER = System.getenv("DB_USER");
+        String DB_PASSWORD = System.getenv("DB_PASSWORD");
+
+        response.setContentType("application/json;charset=UTP-8");
+
+        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+             PreparedStatement stmt = conn.prepareStatement("SELECT menu_name FROM Menu");
+             ResultSet rs = stmt.executeQuery()) {
+
+                while (rs.next()) {
+                    System.out.println(rs.getString("menu_name"));
+                }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            response.setContentType("text/plain;charset=UTF-8");
+            response.getWriter().println("server error: " + e.getMessage());
+        }
     }
 }
