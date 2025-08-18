fcfcd949f7a2242b2c4fd875b86e8147a973ee3e
Min <112598226+jixxxxmin@users.noreply.github.com>
2025-08-18 20:28:59 +0900

    Update GetMenuServlet.java

--- board/src/GetMenuServlet.java
+++ board/src/GetMenuServlet.java
@@ -30,31 +30,63 @@ public class GetMenuServlet extends HttpServlet {
         String DB_USER = System.getenv("DB_USER");
         String DB_PASSWORD = System.getenv("DB_PASSWORD");
 
+        String query;
+
         response.setContentType("application/json;charset=UTF-8");
         List<Map<String, String>> menuList = new ArrayList<>();
+        Map<String, Map<String, Object>> menuIDMap = new HashMap<>();
+
+        query = "SELECT * FROM Menu";
 
         try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-             PreparedStatement stmt = conn.prepareStatement("SELECT menu_name FROM Menu");
+             PreparedStatement stmt = conn.prepareStatement(query);
              ResultSet rs = stmt.executeQuery()) {
 
             while (rs.next()) {
                 Map<String, String> menuMap = new HashMap<>();
-                menuMap.put("menu", rs.getString("menu_name"));
+                menuMap.put("num", rs.getString("menu_id"));
+                menuMap.put("이름", rs.getString("menu_name"));
+                menuMap.put("메뉴", new ArrayList<Map<String, String>>());
                 menuList.add(menuMap);
+
+                menuIDMap.put(menuID, menuMap);
             }
-
-            Gson gson = new Gson();
-            String jsonResponse = gson.toJson(menuList);
-
-            PrintWriter out = response.getWriter();
-            out.print(jsonResponse);
-            out.flush();
         }
-
         catch (Exception e) {
             e.printStackTrace();
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            response.getWriter().println("server error: " + e.getMessage());
+            response.getWriter().println("Get Menu error: " + e.getMessage());
         }
+
+        query = "SELECT * FROM SubMenu";
+
+        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+             PreparedStatement stmt = conn.prepareStatement(query);
+             ResultSet rs = stmt.executeQuery()){
+            
+            while (rs.next()) {
+                String menuID = rs.getString("menu_id");
+                Map<String, Object> parentMenu = menuIDMap.get(menuID);
+
+                Map<String, String> menuMap = new HashMap<>();
+                menuMap.put("num", rs.getString("submenu_id"));
+                menuMap.put("이름", rs.getString("submenu_name"));
+                menuMap.put("설명", rs.getString("descript"));
+
+                ((List<Map<String, String>>) parentMenu.get("메뉴")).add(menuMap);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().println("Get SubMenu error: " + e.getMessage());
+        }
+        
+        Gson gson = new Gson();
+        String jsonResponse = gson.toJson(menuList);
+
+        PrintWriter out = response.getWriter();
+        out.print(jsonResponse);
+        out.flush();
     }
 }
